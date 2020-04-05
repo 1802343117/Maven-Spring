52640d4641d6644241c3706be953d3717e574963
Tanxx <1255965856@qq.com>
2020-04-05 13:13:35 +0800

    用户登入后增加积分

--- webComment/src/test/java/com/soft1851/spring/web/service/MusicServiceTest.java
+++ webComment/src/test/java/com/soft1851/spring/web/service/MusicServiceTest.java
@@ -1,7 +1,6 @@
 package com.soft1851.spring.web.service;
 
 import com.soft1851.spring.web.config.SpringDataSourceConfig;
-import com.soft1851.spring.web.entity.Music;
 import com.soft1851.spring.web.spider.MusicSpider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -21,12 +21,9 @@ public class MusicServiceTest {
 
     @Test
     public void batchInsert() {
-        for (Music music: MusicSpider.musicSpider()) {
-            if(music.getTitle() != null && music.getUrl() != null && music.getSinger() != null && music.getCover() != null) {
-                int []ints = musicService.batchInsert(MusicSpider.musicSpider());
-                assertEquals(MusicSpider.musicSpider().size(), ints.length);
-                System.out.println("数据已全部插入");
-            }
-        }
+
+        int[] ints = musicService.batchInsert(MusicSpider.musicSpider());
+        assertEquals(MusicSpider.musicSpider().size(), ints.length);
+
     }
 }
